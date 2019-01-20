bacaf7cc68e4c9a9662e7abe875a8f8ae31f1282
aschenxiaoran <as_chenxiaoran@163.com>
2019-01-20 22:19:03 +0800

    first commit

---
+++ gofun-ms-carservice/src/test/java/gofun/ms/carservice/GofunMsCarserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package gofun.ms.carservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GofunMsCarserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
